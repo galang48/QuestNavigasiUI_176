b3731e77e4fae33810bdcca46154874d529ae567
galang48 <galangyudha1945@gmail.com>
2025-11-07 09:55:19 +0700

    Menambahkan Composable FormIsian dengan jenisK & OnSubmitBtnClick

---
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -0,0 +1,11 @@
+package com.example.navigasiku.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki","Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+
+}
