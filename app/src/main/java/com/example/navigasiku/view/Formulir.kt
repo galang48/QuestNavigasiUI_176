6fd5e652164f1b3d21736ab0687e39bc204a6439
galang48 <galangyudha1945@gmail.com>
2025-11-07 10:30:37 +0700

    tambah HorizontalDivider merah 1dp

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -65,6 +65,12 @@ fun FormIsian(
                     }
                 }
             }
+            HorizontalDivider(modifier = Modifier
+                .padding(20.dp)
+                .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
         }
 
     }
